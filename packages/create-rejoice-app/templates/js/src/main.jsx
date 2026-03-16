b0871ef6dbd971ec3800d7a16634dbd6750e98d1
Aviroop Paul <aviroop.paul@think41.com>
2026-03-16 12:48:16 +0530

    chore: remove unused js template

--- packages/create-rejoice-app/templates/js/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import { createRoot, RejoiceProvider } from "rejoice-js";
-import App from "./App";
-
-createRoot(document.getElementById("root")).render(
-  <RejoiceProvider defaultTheme="{{defaultTheme}}">
-    <App />
-  </RejoiceProvider>
-);
